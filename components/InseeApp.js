020d985789a6021a4c1711c7c6d22064668cd435
Florian Cassayre <florian.cassayre@gmail.com>
2021-08-22 10:51:42 +0200

    Initial commit

---
+++ components/InseeApp.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { App } from '../../insee-deces-front';
+
+export function InseeApp() {
+  return (
+    <App />
+  );
+}
---
+++ pages/insee.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { InseeApp } from '../components';
+
+export default function Insee() {
+  return (
+    <InseeApp />
+  )
+};
